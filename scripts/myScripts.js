21c13b32b0ac8cc88bbe81a902cf0bdc2a43d8b3
PellDEV <pedroasfelipe@gmail.com>
2024-12-09 11:39:36 -0300

    Front end praticamente pronto poxima etapa guardar as informações na memoria do navegador

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -14,36 +14,38 @@ function taskDone() {
  }
 
  // FUNÇÃO ADICIONAR TAREFA 
-
-//  function newTask () {
-//     let task = document.getElementById('outTask')
-//     task.innerHTML = `<div class="task-card input-card">
-//                 <div class="ico-check icon" onclick="taskDone()" id="outChecked"> <img src="../img/check-off.png" alt="check-off icon" ></div>
-//                 <div class="task" id="outTask"> <p>Retirar o lixo</p></div>
-//                 <div class="hour" id="outHour">10 as 11</div>
-//                 <div class="ico-delete icon" onclick="deleteTask()"> <img src="../img/delete.png" alt="delete-item"></div>
-//                 </div> `;
-       
-//  }
-
  function newTask() {
     let taskContainer = document.getElementById('outTask');
-    let newTaskHTML = `
+    let inTaskElement = document.getElementById('in-Tarefa');
+    let inTimeElement = document.getElementById('in-Hora');
+
+    let inTask = inTaskElement.value
+    let inTime = inTimeElement.value
+
+    if (inTask === "" || inTime === "" || isNaN(inTime) ) {
+        alert("Preencha todos os campos de forma válida"); 
+        return} 
+
+        let newTaskHTML = `
         <div class="task-card input-card">
             <div class="ico-check icon" onclick="taskDone(this)"> 
                 <img src="../img/check-off.png" alt="check-off icon">
             </div>
             <div class="task">
-                <p>Retirar o lixo</p>
+                <p> ${inTask}</p>
             </div>
-            <div class="hour">10 as 11</div>
+            <div class="hour">${inTime}:hrs</div>
             <div class="ico-delete icon" onclick="deleteTask(this)"> 
                 <img src="../img/delete.png" alt="delete-item">
             </div>
         </div>`;
-    
+
     taskContainer.innerHTML += newTaskHTML;
-}
+
+    inTaskElement.value ="";
+    inTimeElement.value ="";
+    }
+  
 
 function taskDone(element) {
     element.innerHTML = '<img src="../img/check-on.svg" alt="checked-on icon">';
@@ -58,3 +60,5 @@ function taskNotDone(element) {
 function deleteTask(element) {
     element.parentElement.remove();
 }
+
+

91b49f33a880ddae47aec609699204426f0a55af
PellDEV <pedroasfelipe@gmail.com>
2025-05-12 14:34:53 -0300

    new version 1.01

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -1,3 +1,37 @@
+function addTask() {
+    let task = document.getElementById('in-Tarefa').value;
+    let data = document.getElementById('in-Data').value;
+    let outList = document.getElementById('outList');
+    let taskList = "";
+    if (task == "") {
+        alert('Digite uma tarefa!');
+        return;
+    } 
+    if (data == "") {
+        alert('Digite uma data!');
+        return;
+    }       
+    taskList = `
+        <div class="task-card input-card">
+            <div class="ico-check icon" onclick="taskDone()" id="outChecked"> <img src="../img/check-off.png" alt="check-off icon" ></div>
+            <div class="task" id="outTask"> <p>${task}</p></div>
+            <div class="hour" id="outHour">${data}</div>
+            <div class="ico-delete icon" onclick="deleteTask()"> <img src="../img/delete.png" alt="delete-item"></div>
+        </div>
+    `;
+    
+    outList.innerHTML += taskList;
+     document.getElementById('in-Tarefa').value = '';
+     document.getElementById('in-Data').value = '';
+
+}
+function deleteTask() {
+    let outList = document.getElementById('outList');
+    let taskCard = event.target.closest('.task-card');
+    if (taskCard) {
+        outList.removeChild(taskCard);
+    }
+}
 
 // FUNÇÃO QUE MARCA A CAIXINHA TAREFA FEITA
 function taskDone() {
@@ -5,7 +39,7 @@ function taskDone() {
      checked.innerHTML = '<img src="../img/check-on.svg" alt="checked-on icon">';
      checked.setAttribute('onclick', 'taskNotDone()');
  }
- 
+
  // FUNÇÃO QUE DESMARCA A CAIXINHA TAREFA FEITA
  function taskNotDone() {
      let unChecked = document.getElementById('outChecked');
@@ -14,4 +48,3 @@ function taskDone() {
  }
 
  //
- 
